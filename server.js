869ee56663fd6bf8ae9b1551d4e181586b55f6a2
Andrew Rodriguez <andrewrb22@gmail.com>
2020-10-30 18:26:42 -0400

    updated servre.js PORT

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const PORT = 3000;
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
 const app = express();
+const db = require("./models");
+
 app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
+
 app.use(express.static("public"));
+
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-// routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
+// Routes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
