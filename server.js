6a59288312ceefb44c2b87dbf9417c2f5dedb5df
Andrew Rodriguez <andrewrb22@gmail.com>
2020-10-30 20:53:01 -0400

    deploying to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
@@ -22,5 +22,5 @@ mongoose.connect(
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}`);
 });
